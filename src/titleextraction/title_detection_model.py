519b482c188428c4fba284b2df28cc3abb1777d2
aaj-fullfact <60602733+aaj-fullfact@users.noreply.github.com>
2023-12-14 01:42:45 +0000

    rewrote docstrings in google format

--- src/titleextraction/title_detection_model.py
+++ src/titleextraction/title_detection_model.py
@@ -23,7 +23,16 @@ class Title_Detection_Model:
         self.all_extractors = [self.binary_extractor, self.pdf_loc_feature_extractor, self.text_embedding_extractor]
 
     def extract_features_from_record(self, record: Dict) -> np.array:
-        """use feature extractors to extract all relevant features from a record and combine in numpy array"""
+        """use feature extractors to extract all relevant features from a record and combine in numpy array
+
+            Args:
+                Dict: this is a record from a pandas dataframe containing the following keys:
+
+                [Text	IsBold	IsItalic	IsUnderlined	Left	Right	Top	  Bottom	 FontType	Label]
+
+            Returns: A numpy array containing the feature representation of the data sample
+
+        """
 
         concatenated_features = np.array([])
         for extractor in self.all_extractors:
@@ -32,12 +41,28 @@ class Title_Detection_Model:
         return concatenated_features
 
     def load_datasets(self):
+
+        """Load the train and test datasets located in the data folder
+
+            Args:
+            Returns: A dictionary in the format {"train": train DataFrame, "test": test DataFrame}
+
+        """
         dataset_dict = {"train": pd.read_csv("data/train_sections_data.csv", header=0, encoding='unicode_escape').sample(frac=1).reset_index(drop=True),
                         "test": pd.read_csv("data/test_sections_data.csv", header=0, encoding='unicode_escape').sample(frac=1).reset_index(drop=True)}
         return dataset_dict
 
     def create_feature_dataset(self, df) -> Dict:
-        """Convert a train or test dataset into a feature set of numerical representations"""
+        """Convert a train or test dataset into a feature set of numerical representations
+
+            Args:
+                df: this is a pandas dataframe containing data with in the following format as
+
+                [Text	IsBold	IsItalic	IsUnderlined	Left	Right	Top	  Bottom	 FontType	Label]
+
+            Returns: A dictionary in the format {"x":list contatining records converted to features, "y": list of labels}
+
+        """
         output_list = []
 
         for id, record in df.iterrows():
@@ -47,17 +72,32 @@ class Title_Detection_Model:
 
 
     def train_MLP(self, feature_dataset):
-        """Train a multi layer perceptron on a feature set of numerical representations"""
+        """Train a multi layer perceptron on a feature set of numerical representations
+
+            Args:
+                feature_dataset: A numpy array of dataset records that have be converted in to feature representations
+
+            Returns: clf: The Trained MLP
+
+        """
         clf = MLPClassifier(random_state=42, max_iter=300).fit(feature_dataset["x"], feature_dataset["y"])
         return clf
 
     def predict_model(self, feature_set, clf):
-        """Predict model on feature set"""
+        """Predict model on feature set
+
+            Args:
+                feature_set: A numpy array of dataset records that have be converted in to feature representations
+                clf: A trained Multi layer Perceptron
+
+            Returns: A numpy array of Model predictions
+
+        """
         return clf.predict(feature_set)
 
     def evaluate_model(self, test_feature_dataset, clf) -> str:
         """Evaluate the model on a feature set. Returns a classification report detailing F1 and accuracy
-            returns:
+            Returns:
                 F1 classification report: str
         """
         preds = self.predict_model(test_feature_dataset["x"], clf)
@@ -67,7 +107,11 @@ class Title_Detection_Model:
 
 
     def end_to_end_training(self):
-        """Performs end to end training of an mlp model. loades dataset, processes data and evaluates the model"""
+        """Performs end to end training of an mlp model. loades dataset, processes data and evaluates the model
+
+            Returns: trained_model: The Trained MLP
+
+        """
         _logger.info("loading dataset")
         dataset_dict = self.load_datasets()
         _logger.info(" dataset loaded")
